59eb953cc7e3d603275869c95c00071cb948560e
闫政 <zyan1226@163.com>
2022-11-25 18:56:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img src="./assets/beams.jpg" alt="" class="top-1/2 left-1/2 max-w-none -translate-x-1/2 
+  -translate-y-1/2 w-full h-screen fixed" />
+  <div id="grid" class="fixed inset-0 bg-center 
+  [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]">
+  </div>
+  <AboutMe />
+</template>
+
+<script>
+import AboutMe from './pages/AboutMe.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AboutMe
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#grid {
+  background-image: url(./assets/grid.svg);
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div class="lg:w-7/12 md:w-9/12 bg-white md:mx-auto md:my-20 mx-5 md:p-20
+     p-10 rounded-3xl break-words my-5 shadow-xl ring-1 ring-gray-900/5 opacity-95 backdrop-blur-3xl">
+      <div class="flex flex-row items-center">
+        <p class="text-5xl font-semibold">{{ modelData.name }}</p>
+        <img src="../assets/avatar.png" alt="" class="w-14 h-14 ml-auto">
+      </div>
+      <div class="mt-5 flex flex-row items-center">
+        <a class="link red-link" href="https://blog.zyan1226.cn">博客</a>
+        <a class="link green-link" href="https://juejin.cn/user/3702810895726344/posts">掘金</a>
+        <a class="link purple-link" href="https://gitee.com/zyan1226">Gitee</a>
+        <a class="link rose-link" href="https://unsplash.com/@zyan1226">Unsplash</a>
+        <a class="link gray-link" href="https://space.bilibili.com/124493907">bilibili</a>
+      </div>
+      <div v-for="item in modelData.content" :key="item.title">
+        <p class="text-2xl font-semibold mt-10">{{ item.title }}</p>
+        <p class="mt-5 text-lg" v-for="(contentItem,index) in item.content" :key="index">{{ contentItem }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ModelData } from '../model/ModelData'
+
+export default {
+  name: 'AboutMe',
+  data() {
+    return {
+      modelData: ModelData
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.link {
+  @apply text-lg px-3 py-2 font-semibold rounded-md mr-3;
+  transition: 750ms;
+}
+
+.red-link {
+  color: #fc3549;
+  background-color: #ffebed;
+}
+
+.red-link:hover {
+  color: white;
+  background-color: #e84950;
+  transition: 750ms;
+}
+
+.green-link {
+  color: #22a3ab;
+  background-color: #dcfdff;
+}
+
+.green-link:hover {
+  color: white;
+  background-color: #68d9e5;
+  transition: 750ms;
+}
+
+.purple-link {
+  color: #7735fc;
+  background-color: #e4dafc;
+}
+
+.purple-link:hover {
+  color: white;
+  background-color: #6f39f2;
+  transition: 750ms;
+}
+
+.rose-link {
+  color: #ed00d4;
+  background-color: #f8d7f2;
+}
+
+.rose-link:hover {
+  color: white;
+  background-color: #d92fcd;
+  transition: 750ms;
+}
+.gray-link{
+  color: #888888;
+  background-color: #f3f3f3;
+}
+.gray-link:hover {
+  color: white;
+  background-color: #888888;
+  transition: 750ms;
+}
+</style>
